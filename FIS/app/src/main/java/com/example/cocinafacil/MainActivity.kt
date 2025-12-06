42b70ce64262d13f515932c741e4bba653dcec5c
Brayan03117 <laragbrayan03@gmail.com>
2025-12-05 22:42:09 -0600

    iniciar sesion

--- FIS/app/src/main/java/com/example/cocinafacil/MainActivity.kt
+++ FIS/app/src/main/java/com/example/cocinafacil/MainActivity.kt
@@ -1,20 +1,54 @@
 package com.example.cocinafacil
 
 import android.os.Bundle
+import android.widget.Button
+import android.window.OnBackInvokedDispatcher
+import androidx.activity.addCallback
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 
 class MainActivity : AppCompatActivity() {
+    private var pantalla=1
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_main)
+        pantallaPrincipal()
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
+
+        onBackPressedDispatcher.addCallback(this){
+            if(pantalla!=1){
+                pantallaPrincipal()
+            }else{
+                finish()
+            }
+        }
+
     }
+    private fun pantallaPrincipal(){
+        setContentView(R.layout.activity_main)
+        pantalla=1
+        val botonInicio=findViewById<Button>(R.id.inicioBoton)
+        botonInicio.setOnClickListener {
+            pantallaLogin()
+        }
+        val botonRegistro=findViewById<Button>(R.id.registraseBoton)
+        botonRegistro.setOnClickListener {
+            pantallaRegistro()
+        }
+    }
+    private fun pantallaLogin(){
+        setContentView(R.layout.iniciasesion)
+        pantalla=2
+    }
+    private fun pantallaRegistro(){
+        setContentView(R.layout.registro)
+        pantalla=3
+    }
+
 }
